69de4072beb8ac4dfdcf7ad222a2f7d161da2e99
clkwrkunvrs <t_raviskeller_32@hotmail.com>
2024-03-25 07:20:44 +0300

    Added ch3 minimum exercise

---
+++ ch3/minimum.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+const minimum = (a,b) => {
+    //return b ^ ((a ^ b) & -(a < b)); //bitwise min
+    return a <= b ? a : b;
+}
+
+assert(minimum(1,2) == 1);
+assert(minimum(2,1) == 1);
+assert(minimum(-1,1) == -1);
+assert(minimum(2,2) == 2);
+assert(minimum(0,0) == 0);
+assert(minimum(-1,-1) == -1);
