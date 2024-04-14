bf6905b82abfe4a60ff95935ff732a52ec387494
clkwrkunvrs <t_raviskeller_32@hotmail.com>
2024-04-14 12:57:15 +0300

    Added helper functions

--- ch4/aList.js
+++ ch4/aList.js
@@ -20,10 +20,26 @@ const listToArray = (list) => {
     return newArr;
 }
 
+const prepend = (elem, list) => {
+    return {
+      value: elem,
+      rest: list
+    }
+}
+
+const nth = (index, list) => {
+    let count = 0; 
+    let listLength = getLengthOfList(list);
+    while (count < listLength && count < index) {
+        list = list.rest;  // Move to the next node
+        count++;  // Increment the count for each node encountered
+    }
+    return (count >= list.length) ? undefined : list;
+}
+
 function getLengthOfList(node) {
     let count = 0;
     while (node !== null) {
-      console.log("node is" + node.value);
         count++;  // Increment the count for each node encountered
         node = node.rest;  // Move to the next node
     }
@@ -33,3 +49,8 @@ function getLengthOfList(node) {
 const testArr = [1,2,3];
 let testList =  arrayToList(testArr);
 listToArray(testList);
+
+let prepended = prepend(4, testList);
+console.log("prepended: " + JSON.stringify(prepended));
+let index = 2;
+console.log("index " + index + " in list is: " + JSON.stringify(nth(index, prepended)));
